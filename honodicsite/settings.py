0169ab0ad73958fb8d51b6e0153bacc34637b74a
Shourov Roy <shourovroy2389@gmail.com>
2024-12-20 19:28:00 +0600

    database config

--- honodicsite/settings.py
+++ honodicsite/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
         "PASSWORD": "shourovtest",
         "HOST": "127.0.0.1",
         "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET default_storage_engine=INNODB",
+        }
     }
 }
 
